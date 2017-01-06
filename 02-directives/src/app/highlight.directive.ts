97badaeb4f104233c838bba147f9bfc37a57df64
Yosi Angel <yosi.angel@gmail.com>
2017-01-06 02:13:50 +0200

    directives complete

---
+++ 02-directives/src/app/highlight.directive.ts
@@ -0,0 +1,41 @@
+import { Directive, HostListener , HostBinding, Input, OnInit } from '@angular/core';
+// import { Directive, ElementRef, Renderer } from '@angular/core';
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective {
+  
+  @HostListener('mouseenter')  mouseover() {
+    this.backgroundColor=this.highlightColor;
+  }
+
+  @HostListener('mouseleave')  mouseleave() {
+    this.backgroundColor=this.defaultColor;
+  }
+
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  }
+  
+  @Input() defaultColor = 'white';
+  @Input('highlight') highlightColor = 'green';
+  private backgroundColor: string;
+
+  ngOnInit()  {
+    this.backgroundColor=this.defaultColor;
+  }
+
+  //private elementRef: ElementRef, private renderer:Renderer
+  constructor() { 
+    // this.elementRef.nativeElement.style.backgroundColor='green';
+    // this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color',
+    // 'green');
+
+    
+
+  }
+
+
+
+}
