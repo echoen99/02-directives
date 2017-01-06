4660bf63ff69c7fe71ba594101ffa4f6eb8f7b15
Yosi Angel <yosi.angel@gmail.com>
2017-01-06 19:37:22 +0200

    with shoping-list service

--- first-app/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ first-app/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
- recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://www.thecherryshare.com/pictures/2718710wienerschnitzel.jpg', []),
-    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
-  ];
+  recipes: Recipe[] = [];
 
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  //recipe = new Recipe('dummy','dummy','http://thumbs2.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg')
-  constructor() { }
+
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit() {
+    this.recipes=this.recipeService.getRecipes();
   }
 
   onSelected(recipe:Recipe) {
